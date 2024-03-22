6e991d87cf68b27721fb3bc7bbc7e73f6530405c
Danilo Meireles <danilo.meireles@gmail.com>
2024-03-21 21:47:35 -0700

    Add OAuthController

---
+++ OAuthDemo.Api/Controllers/OAuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuthDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OAuthController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, GoogleDefaults.AuthenticationScheme);
+        }
+    }
+}
